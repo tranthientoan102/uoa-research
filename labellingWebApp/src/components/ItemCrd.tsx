e32c29b39a118f952eeb1d9082969cba8f7910b0
tom <tranthientoan102@gmail.com>
2022-02-07 03:56:54 +1030

    [web] rebuild

--- labellingWebApp/src/components/ItemCrd.tsx
+++ labellingWebApp/src/components/ItemCrd.tsx
@@ -86,7 +86,7 @@ class ItemCrd extends React.Component<Props> {
             </Flex>)
         else {
             return (
-                <SimpleGrid flexDirection="column" flexWrap="wrap" justify="center" align="center" m={0} p={0}>
+                <SimpleGrid flexDirection="column" flexWrap="wrap" align="center" m={0} p={0}>
                     <Button onClick={() => this.getFnc()} >
                         {this.compTittle}
                     </Button>
