01c3f1ae15aaf1a6e5a9e0a6b0f55eca7329de95
Tom <tranthientoan102@gmail.com>
2022-06-05 18:37:45 +0930

    roll back ED model

--- eventDetection/EDModel.py
+++ eventDetection/EDModel.py
@@ -1,7 +1,7 @@
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
-from transformers import BertTokenizer
+from transformers import BertTokenizer, AutoModel
 
 import detector
 from detector import Detector
@@ -10,29 +10,9 @@ from detector import Detector
 class EDModel:
 
     def __init__(self):
-        logger = TensorBoardLogger("lightning_logs", name="toxic-comments")
-        checkpoint_callback = ModelCheckpoint(
-                dirpath="checkpoints",
-                filename="best-checkpoint",
-                save_top_k=1,
-                verbose=True,
-                monitor="val_loss",
-                mode="min"
-        )
-        early_stopping_callback = EarlyStopping(monitor='val_loss', patience=2)
-
-        trainer = pl.Trainer(
-                logger=logger,
-                # checkpoint_callback=checkpoint_callback,
-                callbacks=[checkpoint_callback, early_stopping_callback],
-                max_epochs=1,
-                gpus=0,
-                # progress_bar_refresh_rate=30
-        )
-        trainer.checkpoint_callback.best_model_path = './checkpoints/best-checkpoint.ckpt'
 
         self.model = Detector.load_from_checkpoint(
-                trainer.checkpoint_callback.best_model_path,
+                './checkpoints/best-checkpoint.ckpt',
                 n_classes=len(detector.LABEL_COLUMNS)
         )
         self.model.eval()
