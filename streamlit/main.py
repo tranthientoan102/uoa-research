370f2365b4f799a8fc58e6cbe06820e3ba65c9fc
Tom <tranthientoan102@gmail.com>
2022-06-19 20:54:56 +0930

    add data visualize + predict

--- streamlit/main.py
+++ streamlit/main.py
@@ -2,11 +2,12 @@ from datetime import datetime
 import re
 
 import bertopic
+import pandas
 import spacy
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
-import plotly
+from plotly import express as px
 import gdown
 from os.path import exists
 
@@ -21,34 +22,48 @@ def getFirebaseService():
     return myFirebase.MyFirebaseService(certPath='./firebase.json'
                      , collectionName='tweets_health')
 
-
 def downloadTweets(myfirebase, kws, fromDate):
-    tmp = []
+    text = []
+    postAt = []
     # st.session_state.status_downloadTweets = True
     print(kws)
     # with st.spinner(f'Downloading data filtered by {kws} from {fromDate} ...'):
     try:
 
-        data = myfirebase.getCollectionRef().where("postAt", ">=", fromDate).stream()
+        data = myfirebase.getCollectionRef()\
+            .where("postAt", ">=", fromDate)\
+            .stream()
 
         for t in data:
             td = t.to_dict()
             if 'text' in td.keys() and checkContainKws(kws, td['text']):
-                tmp.append({'text':td['text'],'postAt':td['postAt'].isoformat()})
-
+                # tmp.append({'text':td['text'],'postAt':td['postAt'].isoformat()})
+                text.append(td['text'])
+                postAt.append(pd.Timestamp(td['postAt'].isoformat()))
     except Exception as e:
         print(e.__str__())
 
+    st.session_state.status_downloadTweets = True
 
+    # st.session_state.df =
+    # st.session_state.tweets = tmp
+    # st.session_state.rawTweets = list(map(lambda x: x['text'], tmp))
+    # st.session_state.cleanTweets = list(map(lambda x: cleaning(x['text'],nlp), tmp))
 
+    result = pandas.DataFrame({'raw':text
+                                , 'clean': list(map(lambda x: cleaning(x, nlp), text))
+                                , 'postAt': postAt}
+                              )
+    print(result)
+    #
+    # st.success(f'Downloaded {len(st.session_state.tweets)} tweets filtered by {kws} from {fromDate}')
 
-    st.session_state.tweets = tmp
-    st.session_state.rawTweets = list(map(lambda x: x['text'], tmp))
-    st.session_state.cleanTweets = list(map(lambda x: cleaning(x['text'],nlp), tmp))
+    # return text,postAt
+    return result
 
-    st.success(f'Downloaded {len(st.session_state.tweets)} tweets filtered by {kws} from {fromDate}' )
-
-    # return tmp
+@st.experimental_singleton
+def downloadDataset():
+    return downloadTweets(getFirebaseService(), ['ausvot','auspol'], datetime.strptime('01-04-2022','%d-%m-%Y'))
 
 def checkContainKws(kws, text):
     if len(kws)==0: return True
@@ -99,21 +114,21 @@ def cleaningStringList(stringList, cleanFunc=cleaning):
         result.append(cleanFunc(s, nlp))
     return result
 
+@st.experimental_singleton
+def loadTrainedBerTopicModel_clean():
+    # if not exists(f'./models/{modelName}'):
+    #     embedding_model = SentenceTransformer('distilbert-base-nli-mean-tokens')
+    #     model = BERTopic(embedding_model=embedding_model, calculate_probabilities=True, verbose=True, n_gram_range=(1,2))
+    #     if modelName == 'tweetBerTopic_clean':
+    #         _ = model.fit_transform(st.session_state.cleanTweets)
+    #     else: _ = model.fit_transform(st.session_state.rawTweets)
+    #     model.save(f'./models/{modelName}')
 
-def loadTrainedBerTopicModel(modelName='tweetBerTopic_clean'):
-    if not exists(f'./models/{modelName}'):
-        embedding_model = SentenceTransformer('distilbert-base-nli-mean-tokens')
-        model = BERTopic(embedding_model=embedding_model, calculate_probabilities=True, verbose=True, n_gram_range=(1,2))
-        if modelName == 'tweetBerTopic_clean':
-            _ = model.fit_transform(st.session_state.cleanTweets)
-        else: _ = model.fit_transform(st.session_state.rawTweets)
-        model.save(f'./models/{modelName}')
+    return bertopic.BERTopic.load(f'./models/tweetBerTopic_clean')
 
-    return bertopic.BERTopic.load(f'./models/{modelName}')
-
-
-
-    # return bertopic.BERTopic.load('./tweetBerTopic_20220616')
+@st.experimental_singleton
+def loadTrainedBerTopicModel_raw():
+    return bertopic.BERTopic.load(f'./models/tweetBerTopic_raw')
 
 
 
@@ -131,6 +146,7 @@ def main():
 
     # st.sidebar.text()
 
+    st.session_state.df = None
     st.session_state.tweets = []
     st.session_state.status_downloadTweets = False
 
@@ -138,61 +154,124 @@ def main():
 
     myfirebase = getFirebaseService()
 
+    selected = 'trained with cleaned text'
     with st.sidebar:
-        section = option_menu("Section"
-                                , ['explore models', 'predict']
-                                , icons=['circle-fill', 'circle-fill']
+        section = option_menu("Sections"
+                                , ['dataset', 'explore models',  'similar topics']
+                                # , icons=['circle-fill', 'circle-fill']
                                 , menu_icon="book", default_index=0)
 
-        selected = st.radio("Select model"
-                                , ['trained with cleaned text', 'trained with raw text']
-                                # , icons=['check-circle-fill', 'circle-fill']
-                                # , menu_icon="book"
-                                # , default_index=0
+        if section != 'dataset':
+            selected = st.radio("Select model"
+                                    , ['trained with cleaned text', 'trained with raw text']
+                                    # , icons=['check-circle-fill', 'circle-fill']
+                                    # , menu_icon="book"
+                                    # , default_index=0
 
-                               )
+                                   )
     modelName = 'tweetBerTopic_clean' if selected == 'trained with cleaned text' else 'tweetBerTopic_raw'
-    with st.spinner(f'Load BerTopic model...'):
-        tweetBerTopic = loadTrainedBerTopicModel(modelName)
-
 
     if section == 'explore models':
 
+        # with st.spinner(f'Load BerTopic model...'):
+        #     tweetBerTopic = loadTrainedBerTopicModel(modelName)
+        # st.success(f'Model {modelName} is loaded')
 
+        tweetBerTopic = loadTrainedBerTopicModel_clean() if modelName == 'tweetBerTopic_clean' else loadTrainedBerTopicModel_raw()
 
-
-        st.success(f'Model {modelName} is loaded')
-        st.table(tweetBerTopic.get_topic_info()[:20])
-        st.plotly_chart(tweetBerTopic.visualize_hierarchy(top_n_topics=20))
+        st.header('Overview')
         st.plotly_chart(tweetBerTopic.visualize_topics())
         st.plotly_chart(tweetBerTopic.visualize_barchart(height=400, width=300, n_words=10))
 
+        st.header('Details')
+        totalTopics = len(tweetBerTopic.get_topic_info())
+        totalTopics
+        showingTopics = st.selectbox('Number of top topics to display: '
+                                     , options= list(range(0, totalTopics))
+                                     , index=min(30,totalTopics-2)
+                                     )
+
+        df = tweetBerTopic.get_topic_info()[:showingTopics].set_index('Topic')
+        for id in df.index:
+            # print(f'{id}, {df[id]}')
+            name = ', '.join(list(map(lambda x: x[0], tweetBerTopic.get_topic(id))))
+
+            df.loc[id, 'Keywords'] = name
+
+        # df.Name = list(map(lambda x: ', '.join(list(map(lambda y: y[0], x)))
+        #               , df
+        #               ))
+
+        st.table(df)
+        st.plotly_chart(tweetBerTopic.visualize_hierarchy(top_n_topics=showingTopics))
 
 
 
 
 
-    elif section == 'predict':
-        with st.form('initForm'):
-            input_kws = st.text_input('Filter by:', value='ausvot,auspol'
-                                      # , disabled=True
-                                      )
-            lastDate = st.date_input('From:', value=datetime.strptime('01-04-2022', '%d-%m-%Y'))
 
-            # button = st.button('Get data', on_click=(downloadTweets), args=(myfirebase, input_kws.split(','),datetime.fromisoformat(lastDate.isoformat())))
-            button = st.form_submit_button('Get data')
 
-        if button:
-            with st.spinner(f'Downloading data filtered by {input_kws} from {lastDate} ...'):
-                downloadTweets(myfirebase, input_kws.split(','), datetime.fromisoformat(lastDate.isoformat()))
+    elif section == 'dataset':
+        # with st.form('initForm'):
+        #     input_kws = st.text_input('Filter by:', value='ausvot,auspol'
+        #                               # , disabled=True
+        #                               )
+        #     lastDate = st.date_input('From:', value=datetime.strptime('01-04-2022', '%d-%m-%Y'))
+        #
+        #     # button = st.button('Get data', on_click=(downloadTweets), args=(myfirebase, input_kws.split(','),datetime.fromisoformat(lastDate.isoformat())))
+        #     button = st.form_submit_button('Get data')
+        #
+        # if button:
+        #     with st.spinner(f'Downloading data filtered by {input_kws} from {lastDate} ...'):
+        #         downloadTweets(myfirebase, input_kws.split(','), datetime.fromisoformat(lastDate.isoformat()))
 
-        # if st.session_state.refreshReview:
-        st.text_area('Samples'
-                     , value='\n-----\n\n'.join(list(map(lambda x: x['text'], st.session_state.tweets[:5])))
-                     , disabled=True
-                     , height=250
-                     )
 
+        with st.spinner(f'Downloading data...'):
+            fullDf = downloadDataset()
+        st.header('Data sample')
+        st.table(fullDf[:20])
+
+        # st.table(fullDf.groupby(['postAt']).count().reset_index())
+        groupByWeekDf = fullDf.groupby(pd.Grouper(key='postAt',freq='D')).count().reset_index()
+
+        st.header('Data distribution')
+        # st.line_chart(fullDf.groupby(pd.Grouper(key='postAt', freq='W')).count().reset_index())
+        fig = px.bar(groupByWeekDf, x="postAt", y="raw")
+        st.plotly_chart(fig, use_container_width=True)
+
+
+    elif section == 'similar topics':
+
+
+
+        # with st.spinner(f'Load BerTopic model...'):
+        #     tweetBerTopic = loadTrainedBerTopicModel(modelName)
+        # st.success(f'Model {modelName} is loaded')
+        tweetBerTopic = loadTrainedBerTopicModel_clean() if modelName == 'tweetBerTopic_clean' else loadTrainedBerTopicModel_raw()
+        fullTopic = tweetBerTopic.get_topic_info().set_index('Topic')
+        for id in fullTopic.index:
+            newName = ', '.join(list(map(lambda x: x[0], tweetBerTopic.get_topic(id))))
+            fullTopic.loc[id,'Name'] = newName
+
+        st.header('Similar topics from keywords')
+        # listTopics = st.multiselect('Topic to display'
+        #                                 , options=['a']
+        #                             )
+
+        kw = st.text_input('')
+        kwTopics, kwProbs = tweetBerTopic.find_topics(kw, top_n=10)
+        filterDf = fullTopic.loc[kwTopics, :]
+
+        # filterDf.assign({'Prob': prob})
+        filterDf['Prob'] = kwTopics
+
+        st.table(filterDf)
+
+        st.header('Similar topics from text')
+        text = st.text_input('',key='predictText')
+        textTopic, textProb = tweetBerTopic.transform([text])
+        textFilterDf = fullTopic.loc[textTopic, :]
+        st.table(textFilterDf)
 
 if __name__ == "__main__":
     main()
